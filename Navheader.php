7bcaf6d677cba79042f0bc02c9b8037c87286a06
ระพีพัฒน์ วรรณสำราญ <mink69875@gmail.com>
2024-05-26 18:39:17 +0700

    Main menu color

--- Navheader.php
+++ Navheader.php
@@ -2,11 +2,17 @@
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+          <img src="./src/assets/RapeeMEd2-removebg-preview (1).png" alt="" class="bi me-0" width="80" height="60" role="img"">
+        </a>
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <img src="./src/assets/รูปโลโก้แผนก.png" alt="" class="bi me-2" width="60" height="60" role="img"">
+        </a>
+        <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 text-white text-decoration-none">
+          <img src="./src/assets/305582063_514973683964201_2501104888521175544_n-removebg-preview (3).png" alt="" class="bi me-2" width="56" height="56" role="img"">
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Basic PC Building Simulator</a></li>
+          <li><a href="#" class="nav-link px-2 --bs-body-color fs-4" style="color:white;">Basic PC Building Simulator</a></li>
         </ul>
 
 
