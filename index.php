b9778298ecb375f86cfcfa2ea57d46f24c5f6ee5
ระพีพัฒน์ วรรณสำราญ <mink69875@gmail.com>
2024-05-26 17:40:59 +0700

    Main menu ui

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<?php
+include "./conectdb.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PC Building Simulator</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php
+        include "./Navheader.php"    
+    ?>
+
+<div class="container text-center menu">
+  <div class="row">
+    <div class="col">
+      <form action="" method="post">
+      <div class="d-grid gap-2 col-6 mx-auto">
+      <button type="submit" class="btn btn-primary btn-lg">Start Game</button>
+      </div>
+        
+      </form>
+    </div>
+    <div class="col">
+    <form action="" method="post">
+        <div class="d-grid gap-2 col-6 mx-auto">
+        <button type="submit" class="btn btn-secondary btn-lg">View PC</button>
+        </div>
+    
+    </form>
+    </div>
+  </div>
+  
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
