16a9d070008d2a6e659cdcc7bf6f260aa556a9f5
Mxia_Cordelia <mink69875@gmail.com>
2024-07-20 20:42:25 +0700

    Added slide picture

--- index.php
+++ index.php
@@ -34,24 +34,47 @@ include "./conectdb.php";
     </div>
   </div>
 </div>
-  <div class="container mt-5">
+  <div class="container mt-5 mb-5 ">
     <div class="row">
       <div class="col-4 col-sm-3"></div>
       <div class="col-4 col-sm-6">
-        <div class="card mb-3 rounded-3">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="..." class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">คอมพิวเตอร์ คืออะไร?</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <a href="<?php echo $url; ?>infomation.php" class="btn btn-primary text-end mb-2">อ่านเพิ่มเติม</a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./src/assets/Game BG2.png" class="d-block w-100 imgslide rounded-4" alt="...">
+      <div class="carousel-caption d-none d-md-block text-black">
+        <h5><a href="">คอมพิวเตอร์ คืออะไร?</a></h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
       </div>
       <div class="col-4 col-sm-6"></div>
     </div>
